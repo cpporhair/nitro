85ec9704abfe9e8dba29d945c4439489b72e017a
hunan <nan.hu@esgyn.cn>
2026-06-12 15:52:19 +0800

    nitro: inconel: 050 phase D - add runtime operations

---
+++ apps/inconel/runtime/operations.hh
@@ -0,0 +1,41 @@
+#ifndef APPS_INCONEL_RUNTIME_OPERATIONS_HH
+#define APPS_INCONEL_RUNTIME_OPERATIONS_HH
+
+#include <string_view>
+#include <utility>
+
+#include "../core/batch_carrier.hh"
+#include "../core/registry.hh"
+#include "../pipeline/point_get.hh"
+#include "../value/sender.hh"
+#include "../write_path/write_batch.hh"
+#include "./facade.hh"
+
+namespace apps::inconel::rt {
+
+    template <typename NvmeProvider = value::local_nvme_provider>
+    [[nodiscard]] inline auto
+    write_batch(core::client_batch_buffer&& input,
+                NvmeProvider value_nvme = {}) {
+        return write_path::write_batch(
+            *core::registry::coord_sched_singleton(),
+            core::registry::fronts_span(),
+            *core::registry::wal_space_singleton(),
+            core::registry::nvme_by_front_owner_span(),
+            std::move(input),
+            value_nvme);
+    }
+
+    template <typename NvmeProvider = value::local_nvme_provider>
+    [[nodiscard]] inline auto
+    point_get(std::string_view key, NvmeProvider value_nvme = {}) {
+        return pipeline::point_get(
+            *core::registry::coord_sched_singleton(),
+            core::registry::fronts_span(),
+            key,
+            value_nvme);
+    }
+
+}  // namespace apps::inconel::rt
+
+#endif  // APPS_INCONEL_RUNTIME_OPERATIONS_HH
